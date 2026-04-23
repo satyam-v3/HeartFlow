b80cbb9663eab2a70b2a451ab2f321cc17743b20
satyam kumar <satyamjsr69@gmail.com>
2026-04-23 13:36:59 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { AnimatePresence, motion } from 'motion/react'
 import HeartsBg from './components/HeartsBg'
 import Intro from './components/Intro'
@@ -7,6 +7,11 @@ import Reveal from './components/Reveal'
 import Final from './components/Final'
 import Card from './components/ui/Card'
 import FutureHope from './components/FutureHope'
+import introImg from '../assets/intro.png'
+import coupleImg from '../assets/reveal.png'
+import finalImg from './assets/final.png'
+import heartImg from '../../assets/heart.png'
+import byeImg from '../assets/final.png'
 
 const App = () => {
 
@@ -19,6 +24,14 @@ const App = () => {
     <FutureHope key='hope' next={() => setStep(prev => prev + 1)} />,
     <Final key='final' />
   ]
+  useEffect(() => {
+    const images = [introImg, coupleImg, byeImg, heartImg]
+
+    images.forEach((src) => {
+      const img = new Image()
+      img.src = src
+    })
+  }, [])
 
   return (
     <div className="relative min-h-screen bg-love overflow-hidden">
